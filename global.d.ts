ea1b244849b09c4bf11aef5c415b3bc15a3941cf
Juan Portilla <juan@rapihogar.com>
2023-08-15 16:54:35 -0300

    delete all

--- global.d.ts
+++
@@ -1,5 +0,0 @@
-export interface RequestBody {
-  document_number: number;
-  document_type: 'DNI' | 'CUIT' | 'CDI' | 'LE' | 'LC' | 'CI Ext.' | 'CUIL' | 'Pasaporte' | 'Otro' | 'Sin Documento' | 'Parto';
-  gender: 'F' | 'M';
-}
